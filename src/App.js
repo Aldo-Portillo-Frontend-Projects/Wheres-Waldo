0b487b3172ef5fb083fb5d0bfa21a47e3c048ff5
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-11 16:08:20 -0500

    Find coordinates of click

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import Header from "./components/Header";
 function App() {
   
 
+  function location(e){
+    console.log(`offset X: ${e.pageX} offsetY: ${e.pageY}`)
+  }
   return(
     <>
 
       <Header />
-      <img src={require("./media/garden.jpeg")} alt=""></img>
+      <img src={require("./media/garden.jpeg")} alt="" onClick={location}></img>
     
     </>
   )
