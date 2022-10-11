490f52877805bea0efccd7bda927e3c2d23560f8
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-11 16:58:53 -0500

    Fix screen resize bug

--- src/App.js
+++ src/App.js
@@ -7,18 +7,22 @@ function App() {
   function location(e){
     //This function is only used for development
     console.log(e)
-    console.log(`offset X: ${e.screenX} offsetY: ${e.screenY}`)
+    console.log(`offset X: ${e.pageX - e.target.offsetLeft} offsetY: ${e.pageY - e.target.offsetTop}`)
   }
 
   const globe = {
-    xMin: 723,
-    xMax: 796,
-    yMin: 331,
-    yMax: 391,
+    xMin: 956,
+    xMax: 1054,
+    yMin: 252,
+    yMax: 328,
   }
 
   function locateTarget(e){
-    if (globe.xMin < e.screenX && e.screenX < globe.xMax && globe.yMin < e.screenY && e.screenY < globe.yMax){
+
+    const x = e.pageX - e.target.offsetLeft
+    const y = e.pageY - e.target.offsetTop
+
+    if (globe.xMin < x && x < globe.xMax && globe.yMin < y && y < globe.yMax){
       console.log('Found globe')
     } else {
       console.log('try again')
