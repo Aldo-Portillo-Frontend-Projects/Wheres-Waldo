1dbfc1e2b90b9378f9f0de2a06e25fa4ba9de772
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-11 15:58:19 -0500

    Render header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="App">
-      WOrks
-    </div>
-  );
+  
+
+  return(
+    <>
+
+      <Header />
+      <img src={require("./media/garden.jpeg")} alt=""></img>
+    
+    </>
+  )
 }
 
 export default App;
